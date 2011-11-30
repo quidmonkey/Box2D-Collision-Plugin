76271e20023a253646ca68d45e98f48854caefa0
quidmonkey <quidmonkey@gmail.com>
2011-11-30 09:23:46 -0500

    Update collision.js

--- collision.js
+++ collision.js
@@ -17,13 +17,13 @@ ig.Box2DEntity.inject({
 	        this.body.entity = this;
 	},
     
-    	collision: function(){
-		
+	collision: function(){
+	
 		this.standing = false;
-			
+		
 		//for each contact edge
-	        for( var edge = this.body.m_contactList; edge; edge = edge.next ) {
-				
+        for( var edge = this.body.m_contactList; edge; edge = edge.next ) {
+			
 			//grab normal force, which is the vector
 			//perpendicular to the surface of contact
 			var normal = edge.contact.m_manifold.normal;
@@ -33,35 +33,36 @@ ig.Box2DEntity.inject({
 			var y = this.pos.y + normal.y.map( 1, -1, 0, 1 ) * this.size.y;
 			var point = { x: x, y: y };
 				
-	            	//if contact is an entity
+	        //if contact is an entity
 			var ent = edge.other.entity;
-	           	if( ent ){
-					//test for groupIndex & mask bits
-					var f1 = ent.shape.m_filter, f2 = this.shape.m_filter;
-					if( f1.groupIndex != f2.groupIndex || f1.groupIndex > 0 || f1.categoryBits == f2.maskBits ){
-						this.collideEntity( ent, point, normal );
-					}
-	            	}
+	        if( ent ){
+				//test for groupIndex & mask bits
+				var f1 = ent.shape.m_filter, f2 = this.shape.m_filter;
+				if( f1.groupIndex != f2.groupIndex || f1.groupIndex > 0 || f1.categoryBits == f2.maskBits ){
+					this.collideEntity( ent, point, normal );
+				}
+	        }
 			//else contact is a collision tile
-	            	else{
-					//calculate points of contact
-					var tile = ig.game.collisionMap.getTile( x, y );
-	                		this.collideTile( tile, point, normal );
-					
-					//if normal force is straight up
-					//entity is standing
-					if( normal.y < 0 ){
-						this.standing = true;
-					}
-	            	}
+        	else{
+				//calculate points of contact
+				var tile = ig.game.collisionMap.getTile( x, y );
+                this.collideTile( tile, point, normal );
+				
+				//if normal force is straight up
+				//entity is standing
+				if( normal.y < 0 ){
+					this.standing = true;
+				}
+			
+        	}
 		}
-    	},
+	},
 	
 	//passes collision entity, point & normal force
 	collideEntity: function( other, point, normal ){},
     
 	//passes the collision tile, point & normal force
-    	collideTile: function( tile, point, normal ){},
+	collideTile: function( tile, point, normal ){},
 	
 	update: function(){
 		this.parent();

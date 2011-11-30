02b83bac38e2c1815101b1b3d02b0cc49475e438
quidmonkey <quidmonkey@gmail.com>
2011-11-30 09:21:14 -0500

    Update collision.js

--- collision.js
+++ collision.js
@@ -12,51 +12,56 @@ ig.module(
 
 ig.Box2DEntity.inject({
 	
-    init: function( x, y, settings ){
-        this.parent( x, y, settings );
-        this.body.entity = this;
-    },
+	init: function( x, y, settings ){
+	        this.parent( x, y, settings );
+	        this.body.entity = this;
+	},
     
-    collision: function(){
+    	collision: function(){
 		
 		this.standing = false;
-		
-		//for each contact edge
-        for( var edge = this.body.m_contactList; edge; edge = edge.next ) {
 			
+		//for each contact edge
+	        for( var edge = this.body.m_contactList; edge; edge = edge.next ) {
+				
 			//grab normal force, which is the vector
 			//perpendicular to the surface of contact
 			var normal = edge.contact.m_manifold.normal;
 			
-            //if contact is an entity
+			//calculate point of contact
+			var x = this.pos.x + normal.x.map( 1, -1, 0, 1 ) * this.size.x;
+			var y = this.pos.y + normal.y.map( 1, -1, 0, 1 ) * this.size.y;
+			var point = { x: x, y: y };
+				
+	            	//if contact is an entity
 			var ent = edge.other.entity;
-            if( ent ){
-				//test for groupIndex & mask bits
-				var f1 = ent.shape.m_filter, f2 = this.shape.m_filter;
-				if( f1.groupIndex != f2.groupIndex || f1.groupIndex > 0 || f1.categoryBits == f2.maskBits ){
-					this.collideEntity( ent, normal );
-				}
-            }
+	           	if( ent ){
+					//test for groupIndex & mask bits
+					var f1 = ent.shape.m_filter, f2 = this.shape.m_filter;
+					if( f1.groupIndex != f2.groupIndex || f1.groupIndex > 0 || f1.categoryBits == f2.maskBits ){
+						this.collideEntity( ent, point, normal );
+					}
+	            	}
 			//else contact is a collision tile
-            else{
-				//calculate points of contact
-				var x = this.pos.x + normal.x.map( 1, -1, 0, 1 ) * this.size.x;
-				var y = this.pos.y + normal.y.map( 1, -1, 0, 1 ) * this.size.y;
-				var tile = ig.game.collisionMap.getTile( x, y );
-				var point = { x: x, y: y };
-                this.collideTile( tile, point, normal );
-				if( normal.y < 0 ){
-					this.standing = true;
-				}
-            }
+	            	else{
+					//calculate points of contact
+					var tile = ig.game.collisionMap.getTile( x, y );
+	                		this.collideTile( tile, point, normal );
+					
+					//if normal force is straight up
+					//entity is standing
+					if( normal.y < 0 ){
+						this.standing = true;
+					}
+	            	}
 		}
-    },
+    	},
 	
-	//passes colliding entity & normal force
-	collideEntity: function( other, normal ){},
+	//passes collision entity, point & normal force
+	collideEntity: function( other, point, normal ){},
     
 	//passes the collision tile, point & normal force
-    collideTile: function( tile, point, normal ){},
+    	collideTile: function( tile, point, normal ){},
 	
 	update: function(){
 		this.parent();

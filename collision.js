5b8fe195d939398dc1327ba2898fefe07deb2c72
quidmonkey <quidmonkey@gmail.com>
2012-01-10 10:27:51 -0500

    Update collision.js

--- collision.js
+++ collision.js
@@ -42,7 +42,7 @@ ig.module(
                     //test for groupIndex & mask bits
                     var f1 = this..shape.m_filter,
                         f2 = ent.shape.m_filter;
-                    if (f1.groupIndex != f2.groupIndex || f1.groupIndex > 0 || f1.categoryBits == f2.maskBits) {
+                    if (!f1.groupIndex || f1.groupIndex != f2.groupIndex || f1.groupIndex > 0 || f1.categoryBits == f2.maskBits) {
                         this.collideEntity(ent, point, normal);
                     }
                 }
